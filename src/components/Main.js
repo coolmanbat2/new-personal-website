4763d879bb03715e4175a893e22f7886989a0983
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2024-04-02 22:49:16 -0400

    First commit with new updated changes on website. Main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,10 @@ import React from "react";
 function Main(props) {
   return (
     <div className="bg-img" id="main">
-      <div className="main">
-        <div className="main-text">Hi, I'm Thanusun.</div>
-        <div className="main-text">I'm a Full-Stack Developer.</div>
+      <div className="main font-design">
+        <div className="big-text main-color">Hey, I'm <div className="secondary-color">Thanusun Thayaparan</div></div>
+        <div className="main-color med-text">I build Full Stack Applications with React, Django, and SQLite</div>
+        <div className="sub-small-text tertiary-color">I like to travel and eat at new restaurants</div>
       </div>
     </div>
   );

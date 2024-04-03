7aff47d0ed29eea2eec0a5be411d00366268574b
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2024-04-02 22:54:28 -0400

    Made adjustments to color themes and text in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,8 @@ function Main(props) {
   return (
     <div className="bg-img" id="main">
       <div className="main font-design">
-        <div className="big-text main-color">Hey, I'm <div className="secondary-color">Thanusun Thayaparan</div></div>
-        <div className="main-color med-text">I build Full Stack Applications with React, Django, and SQLite</div>
+        <div className="big-text main-color">Hey, I'm <span className="secondary-color">Thanusun Thayaparan</span></div>
+        <div className="main-color med-text">I build Full Stack Applications with <span className="secondary-color">React</span>, <span className="secondary-color">Django</span>, and <span className="secondary-color">SQLite</span></div>
         <div className="sub-small-text tertiary-color">I like to travel and eat at new restaurants</div>
       </div>
     </div>

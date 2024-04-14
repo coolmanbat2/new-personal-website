32a6306a281e7e5240b0a4de73891ea48377a632
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2024-04-14 10:09:23 -0400

    Finally updated image to be properly centered.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,12 @@
-
 import "../css/Image.css";
 import profilePic from "../static/profile.png";
 
 function Image() {
-    return (
-        <img src={profilePic} alt="profile" className="profile-img"/>
-    ) 
+  return (
+    <div className="profile-background">
+      <img src={profilePic} alt="profile" className="profile-img" />
+    </div>
+  );
 }
 
 export default Image;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,11 @@
 import "../css/Main.css";
 import React from "react";
-import Projects from "./Projects";
 
 function Main(props) {
   return (
     <div className="title" id="main">
       <div className="main font-design">
-        <div className="big-main-text main-color">Hey, I'm <span className="secondary-color">Thanusun Thayaparan</span></div>
+        <div className="big-main-text main-color">Hey, I'm <span className="secondary-color heavy-weight">Thanusun Thayaparan</span></div>
         <div className="main-color med-main-text">I build Full Stack Applications with <span className="secondary-color">React</span>, <span className="secondary-color">Django</span>, and <span className="secondary-color">SQLite</span></div>
         <div className="sub-small-main-text tertiary-color">I like to travel and eat at new restaurants</div>
       </div>

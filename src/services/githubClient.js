0bbd93a66ef4a680b5d2d9dfffa7587cb2b303af
Thanusun Thayaparan <thanu@Thanusuns-MacBook-Air.local>
2026-01-28 08:50:18 -0500

    Added authentication for github api; enhanced security

--- src/services/githubClient.js
+++ src/services/githubClient.js
@@ -5,10 +5,17 @@ export async function fetchUserRepos(username) {
     throw new Error("GitHub username is required.");
   }
 
+  const headers = {
+    Accept: "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+    Authorization: `Bearer ${process.env.REACT_APP_PERSONAL_ACCESS_TOKEN}`
+  }
+
   const response = await fetch(
     `${GITHUB_API_BASE}/users/${encodeURIComponent(
       username
-    )}/repos?per_page=100&sort=updated`
+    )}/repos?per_page=100&sort=updated`,
+    { headers }
   );
 
   if (!response.ok) {
